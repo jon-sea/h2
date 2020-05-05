d732578c849bfe6dfe28ddd9b69d1fa628bb5fd2
jon-sea <57441917+jon-sea@users.noreply.github.com>
2020-05-04 23:04:40 -0400

    cant redraw bg

--- main.ts
+++ main.ts
@@ -1,10 +1,18 @@
+let x = 5
+let speed = 1
+let xpos;
 screen.width(80)
 let bg = image.create(screen.width, screen.height)
 scene.setBackgroundImage(bg)
 // Any manipulations now done to bg will be shown on
 // the screen
 bg.printCenter('Hi, there!', 0, 5, image.font8)
-for(let x = 0;x<40;x+=10){
-bg.drawLine(x, 0, 100, 100, 1)
+for(let y = 0;y<40;y+=10){
+bg.drawLine(y, 0, 100, 100, 1)
 }
 bg.drawCircle(50,50,50,1)
+game.onUpdate(function () {
+    x += 1
+    xpos = x + speed
+    bg.drawCircle(x,60,4,1)
+})
